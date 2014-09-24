cdea4f3bfc1aa74f60e053c4f70ee886bf279b32
k2works <kakimomokuri@gmail.com>
2014-09-24 14:29:04 +0900

    chapter1:プロビジョニング

--- cookbooks/case01/recipes/app_config.rb
+++ cookbooks/case01/recipes/app_config.rb
@@ -11,5 +11,5 @@ web_app node[:app_name] do
   passenger_default_ruby "/usr/local/rvm/gems/ruby-#{node['rvm']['app_version']}@#{node['rvm']['app_gemset']}/wrappers/ruby"
   docroot "/var/www/#{node[:app_name]}/public"
   server_name "#{node[:app_name]}.#{node[:domain]}"
-  rails_env node[:passenger][:rails_env]
+  rails_env "#{node[:passneger][:rails_env]}"
 end
